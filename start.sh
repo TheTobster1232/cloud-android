30f59e4ca3a7b72e0a7a480efbe609291fad74bb
TheTobster1232 <151861999+TheTobster1232@users.noreply.github.com>
2026-06-23 17:30:36 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Start noVNC
 websockify --web=/usr/share/novnc/ 8080 localhost:5901 &
 
-# Start QEMU with Android-x86
 qemu-system-x86_64 \
   -m 2048 \
   -smp 2 \
   -machine accel=tcg \
   -vga virtio \
   -display vnc=:1 \
-  -hda /android/android.img \
+  -hda /vm/ubuntu.img \
   -net nic \
   -net user
