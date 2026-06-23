a5c42fb3697344c8fa6149d5e266fe1f338b8cfe
TheTobster1232 <151861999+TheTobster1232@users.noreply.github.com>
2026-06-23 20:15:19 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,16 +3,19 @@
 # Start noVNC
 websockify --web=/usr/share/novnc/ 8080 localhost:5901 &
 
-# Run QEMU in pure text mode for stable Android-x86 install
+# Run QEMU with hybrid display:
+# - VNC for graphics
+# - Text console in terminal
+# - No framebuffer crashes
 qemu-system-x86_64 \
   -m 2048 \
   -smp 2 \
   -machine accel=tcg \
-  -nographic \
-  -vga none \
+  -vga std \
+  -display vnc=:1 \
+  -serial mon:stdio \
   -hda /android/android.img \
   -cdrom /android/android.iso \
   -boot d \
   -net nic \
-  -net user \
-  -serial mon:stdio
+  -net user
