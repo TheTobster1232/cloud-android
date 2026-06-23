db559c960e2aa0b7c7cd83d599a4ee5bc8e659ad
TheTobster1232 <151861999+TheTobster1232@users.noreply.github.com>
2026-06-23 15:29:22 +0100

    Add start script for noVNC and QEMU with Android-x86

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Start noVNC
+websockify --web=/usr/share/novnc/ 8080 localhost:5901 &
+
+# Start QEMU with Android-x86
+qemu-system-x86_64 \
+  -m 2048 \
+  -smp 2 \
+  -machine accel=tcg \
+  -vga virtio \
+  -display vnc=:1 \
+  -hda /android/android.img \
+  -net nic \
+  -net user \
+  -enable-kvm \
+  -cpu host
